bdd00143500026157b198c6ba67b5de915ceb5fb
Barodah66 <barongoe05@gmail.com>
2025-08-05 12:13:20 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "Knight Bot",
+  botName: "Barodah Bot",
   botOwner: 'Barodah', // Your name
   ownerNumber: '254745730801', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
